9930d28aac924c1614668fecf2f0ac017e6fd479
Ruslan <rusichmr86@gmail.com>
2024-11-05 22:55:49 +0400

    rick

--- src/vpn/vpn.service.ts
+++ src/vpn/vpn.service.ts
@@ -6,6 +6,6 @@ export class VpnService {
     return 'xyi';
   }
   tiPidor(): string {
-    return 'Ты пидор';
+    return '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=qE5D2jnwCHLPGhHL" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
   }
 }

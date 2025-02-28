84d252ff9a95691a720b7541e6cbe593c7e40e28
Ruslan <rusichmr86@gmail.com>
2025-02-28 14:13:51 +0400

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as Path from "path";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/app/do')
+  @Render('index')
+  roo2t() {
+    return { message: 'Hello world2' };
   }
 }
